f1d493a78f67ac4ac56de82956391f7cb01b3b5f
Antashyan <lilitantashyan@mail.ru>
2018-10-06 01:16:39 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {IUser} from './user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'users';
+    public loginForm: FormGroup;
+    public users: Array<IUser> = [];
+
+
+    constructor() {
+        this.loginForm = new FormGroup({
+            'userName': new FormControl('',
+                [Validators.required,
+                    Validators.minLength(3),
+                    Validators.pattern(/^[A-Za-z ]{2,30}$/g)]),
+
+            'userSurname': new FormControl('',
+                [Validators.required,
+                    Validators.minLength(3),
+                    Validators.pattern(/^[A-Za-z]+(?:[\s-][A-Za-z]+)*$/g)]),
+
+            'userEmail': new FormControl('',
+                [Validators.required,
+                    Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g)]),
+
+            'userGender': new FormControl('', Validators.required),
+            'password': new FormControl('', Validators.required),
+            'confirmPassword': new FormControl('', Validators.required),
+        });
+
+    }
+
+    public submitForm(): boolean {
+        if (this.loginForm.get('password').value === this.loginForm.get('confirmPassword').value) {
+            this.users.push(this.loginForm.getRawValue());
+            this.loginForm.reset();
+        }
+        return null;
+    }
 }
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,23 @@
+
+import {Component, Input, OnInit} from '@angular/core';
+// @ts-ignore
+import {IUser} from '../user';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  @Input()
+  public user: IUser;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    console.log(this.user);
+  }
+
+}
